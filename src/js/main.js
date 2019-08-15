5ff4482b6e548af48ebdbc23585da677062e3b14
Ilya <mix-xim.zk@ande.ru>
2019-08-15 09:17:54 +0300

    test

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,43 @@
-let callMe = document.querySelector('.navbar__button');
-let modalDialog = document.querySelector('.modal');
-let modalDialogClose = document.querySelector('.modal-dialog__close');
-let check = false;
+// let callMe = document.querySelector('.navbar__button');
+// let modalDialog = document.querySelector('.modal');
+// let modalDialogClose = document.querySelector('.modal-dialog__close');
+// let check = false;
 
-console.log(callMe, modalDialog);
+// console.log(callMe, modalDialog);
 
-function closeModal(){
-    if(check){
-        modalDialog.classList.remove('modal_active');
-    }
-}
+// function closeModal(){
+//     if(check){
+//         modalDialog.classList.remove('modal_active');
+//     }
+// }
 
-callMe.addEventListener('click', () => {
+// callMe.addEventListener('click', () => {
     
-    check = true;
-    modalDialog.classList.add('modal_active'); 
-    setTimeout(closeModal, 1000);
+//     check = true;
+//     modalDialog.classList.add('modal_active'); 
+//     setTimeout(closeModal, 1000);
 
+// });
+// modalDialogClose.addEventListener('click', () => closeModal());
+
+$(document).ready(() => {
+    let button = $('.button');
+    let modal = $('.modal');
+    let close = $('.modal-dialog__close');
+
+    button.on('click', () =>{
+        modal.addClass('modal_active');
+    });
+    close.on('click', () =>{
+        modal.removeClass('modal_active');
+    });
+    $(window).scroll(() => $(this).scrollTop() > 50 ?  $('.btn_up').fadeIn() : $('.btn_up').fadeOut());
+    
+    $('.btn_up').click(() => {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
 });
-modalDialogClose.addEventListener('click', () => closeModal());
-
-
 
