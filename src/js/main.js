1e290e07fc4082b72df89a60c076d5f16455f3e0
Antipenkp-Ilya <mix-xim.zk@yandex.ru>
2019-08-17 20:59:40 +0300

    новые секции и ошибки

--- src/js/main.js
+++ src/js/main.js
@@ -19,7 +19,38 @@
 
 // });
 // modalDialogClose.addEventListener('click', () => closeModal());
+new WOW().init();
+$(document).ready(function(){
+    $('.slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: $('.arrows__left'),
+        nextArrow: $('.arrows__right'),
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
 
+        ]
+    });
+});
+$(document).ready(() => {
+    $('#offer-form').on('submit', () => {
+        event.preventDefault();
+        $.ajax
+    });
+});
 $(document).ready(() => {
     let button = $('.button');
     let modal = $('.modal');
