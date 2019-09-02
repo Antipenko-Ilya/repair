8fe01e7a6bb78da037ac5449384e5cefc9bbb086
Antipenko Iliya <antipinko.iliya@triumph.msk.ru>
2019-09-02 19:51:15 +0300

    НА работе

--- src/js/main.js
+++ src/js/main.js
@@ -92,7 +92,7 @@ $(document).ready(function(){
                 }
             },
             {
-                breakpoint: 768,
+                breakpoint: 865,
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1
